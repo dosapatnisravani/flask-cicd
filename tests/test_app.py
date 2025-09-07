c086f2fa5c9e8c4000d356e7d743233e723701f5
Dosapatni Sravani <sravanidosapatni@gmail.com>
2025-09-07 12:21:07 +0530

    test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -8,4 +8,4 @@ def test_health_endpoint():
     with app.test_client() as c:
         r = c.get("/")
         assert r.status_code == 200
-        assert r.get_json()["status"] == "ok
+        assert r.get_json()["status"] == "ok"
